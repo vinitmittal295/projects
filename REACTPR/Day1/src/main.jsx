fc80b29431aeecf6b6fe1475bf1d774e6603de08
vinit mittal <vinitmittal295@gmail.com>
2024-09-09 16:54:00 +0530

    added

--- REACTPR/Day1/src/main.jsx
+++ REACTPR/Day1/src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
-createRoot(document.getElementById('root')).render(
+const rootvar=document.getElementById('root')
+createRoot(rootvar).render(
   <StrictMode>
     <App />
   </StrictMode>,
