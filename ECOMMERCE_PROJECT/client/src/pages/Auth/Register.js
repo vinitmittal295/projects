70835fe5ae8a1e71d0cd1cefa3797baf659ce122
vinit mittal <vinitmittal295@gmail.com>
2024-09-18 20:50:22 +0530

    added

--- ECOMMERCE_PROJECT/client/src/pages/Auth/Register.js
+++ ECOMMERCE_PROJECT/client/src/pages/Auth/Register.js
@@ -2,18 +2,32 @@
 import React,{useState} from 'react'
 import Layout from '../../components/Layout/Layout'
 import{toast} from "react-toastify"
+import { useNavigate } from 'react-router-dom'
+import axios from "axios"
 const Register = () => {
     const[name,setName]=useState("")
     const[email,setEmail]=useState("")
     const[password,setPassword]=useState("")
     const[phone,setPhone]=useState("")
     const[address,setAddress]=useState("")
+    const navigate=useNavigate()
     // form function
 
-    const handlseSubmit=(e)=>{
+    const handlseSubmit=async (e)=>{
         e.preventDefault()
-        console.log(name,email,password, phone, address)
-        toast.success("resister successflly")
+        try {
+            const res=await axios.post("/api/v1/auth/register",{name,email,password,phone,address})
+            if (res.data.success) {
+                toast.success(res.data.message)
+                navigate("/login")
+            } else {
+                toast.error(res.data.message)
+            }
+        } catch (error) {
+            console.log(error)
+        }
+
+        
 
     }
     
