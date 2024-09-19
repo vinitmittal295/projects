09e354c321b97ea832c1b261d506022df8beefec
vinit mittal <vinitmittal295@gmail.com>
2024-09-19 10:20:39 +0530

    login success

--- ECOMMERCE_PROJECT/client/src/components/Layout/Layout.js
+++ ECOMMERCE_PROJECT/client/src/components/Layout/Layout.js
@@ -2,8 +2,7 @@ import React from 'react'
 import Header from "./Header"
 import Footer from "./Footer"
 import{Helmet} from "react-helmet"
-import { ToastContainer } from 'react-toastify';
-  import 'react-toastify/dist/ReactToastify.css';
+import { Toaster } from 'react-hot-toast'
 // import HomePage from '../../pages/HomePage'
 
 const Layout = ({children,title,description,keywords,author}) => {
@@ -18,7 +17,7 @@ const Layout = ({children,title,description,keywords,author}) => {
             </Helmet>
         <Header/>
         <main style={{minHeight:'70vh'}}>
-        <ToastContainer />
+        <Toaster />
             {children}
         </main>
         <Footer/>
--- ECOMMERCE_PROJECT/client/src/pages/Auth/Login.js
+++ ECOMMERCE_PROJECT/client/src/pages/Auth/Login.js
@@ -1,25 +1,22 @@
-// import { useState } from 'react'
+// import React from 'react'
 import React,{useState} from 'react'
 import Layout from '../../components/Layout/Layout'
 import toast from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom'
 import axios from "axios"
-const Register = () => {
-    const[name,setName]=useState("")
+const Login = () => {
     const[email,setEmail]=useState("")
     const[password,setPassword]=useState("")
-    const[phone,setPhone]=useState("")
-    const[address,setAddress]=useState("")
     const navigate=useNavigate()
-    // form function
+
 
     const handlseSubmit=async (e)=>{
         e.preventDefault()
         try {
-            const res=await axios.post("/api/v1/auth/register",{name,email,password,phone,address})
+            const res=await axios.post("/api/v1/auth/login",{email,password})
             if (res.data.success) {
                 toast.success(res.data.message)
-                navigate("/login")
+                navigate("/")
             } else {
                 toast.error(res.data.message)
             }
@@ -29,29 +26,17 @@ const Register = () => {
             
         }
 
-        
-
     }
-    
   return (
     <Layout title="register-ecommerce App">
     <div className="form-container">
-    <h1>Register page</h1>
+    <h1>Login page</h1>
     <form onSubmit={handlseSubmit}>
-  <div className="mb-3">
-    <input type="text" value={name} onChange={(e)=>setName(e.target.value)} className="form-control" id="exampleInputEmail1" placeholder='enter your name' required/>
-  </div>
   <div className="mb-3">
     <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} className="form-control" id="exampleInputEmail1" placeholder='enter your email' required/>
   </div>
   <div className="mb-3">
     <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} className="form-control" id="exampleInputPassword1"  placeholder='enter password' required/>
-  </div>
-  <div className="mb-3">
-    <input type="text" value={phone} onChange={(e)=>setPhone(e.target.value)} className="form-control" id="exampleInputEmail1" placeholder='enter phone' required />
-  </div>
-  <div className="mb-3">
-    <input type="text" value={address} onChange={(e)=>setAddress(e.target.value)} className="form-control" id="exampleInputEmail1" placeholder='enter address' required/>
   </div>
   <button type="submit" className="btn btn-primary">Submit</button>
 </form>
@@ -62,4 +47,4 @@ const Register = () => {
   )
 }
 
-export default Register
+export default Login
